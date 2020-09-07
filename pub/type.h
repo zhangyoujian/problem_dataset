340874457181c6dc57704af7a28ab1680b8c65f3
Mr zhang <39002861+zhangyoujian@users.noreply.github.com>
2020-09-07 23:14:55 +0800

    Initial commit

---
+++ pub/type.h
@@ -0,0 +1,30 @@
+/*
+Copyright (C), 2020-2050, Tech. Co., Ltd.
+File name: type.h
+Author: Youjian zhang
+Version: 1.0
+Date: 2021.1.2
+Description: heard file
+*/
+#ifndef TYPE_H
+#define TYPE_H
+#include <stdio.h>
+
+
+#ifdef __cpluscplus
+extern "C" {
+#endif
+
+typedef char int8;
+typedef long long int int64;
+
+typedef unsigned int uint;
+typedef unsigned char uint8;
+typedef unsigned long long int uint64;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
