8a497a278d0dd7f206be1e532d289477169162a7
zhangyoujian <1932776119@qq.com>
2020-09-26 23:39:33 +0800

    add solution

---
+++ 3_longestSubstringWithoutRepeatation.hpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> recoder;
+        int curSubstrLen = 0;
+        int maxSubStrLen = 0;
+        for(size_t i = 0; i < s.length(); ++i) {
+            int index = recoder.count(s[i]) == 0 ? -1 : recoder[s[i]];
+            if (index == -1 || (i - index) > curSubstrLen) {
+                curSubstrLen++;
+                if (curSubstrLen > maxSubStrLen) {
+                    maxSubStrLen = curSubstrLen;
+                }
+            } else {
+                curSubstrLen = i - index;
+            }
+            recoder[s[i]] = i;
+        }
+        return maxSubStrLen;
+    }
+};
--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        printf("Usage: a b.\n");
-        return 1;
-    }
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-    printf("%d + %d = %d\n", a, b, a + b);
+    
     return 0;
 }
