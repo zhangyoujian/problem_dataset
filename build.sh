c036f938ae15eeeab5d7ccf4eec1a91686db290d
zhangyoujian <1932776119@qq.com>
2020-10-16 23:47:11 +0800

    fit reposity

--- build.sh
+++ build.sh
@@ -4,15 +4,17 @@ CUR_DIR=$(cd `dirname $0`;pwd)
 BUILD_DIR=$CUR_DIR/build/
 OUTPUT_DIR=$CUR_DIR/output/
 
+rm -rf $OUTPUT_DIR/**
 rm -rf $BUILD_DIR/**
 
 mkdir -p $BUILD_DIR/tmp
 mkdir -p $OUTPUT_DIR/bin
 mkdir -p $OUTPUT_DIR/lib
 
-cd $OUTPUT_DIR/tmp/
-cmake ../../
+cd $BUILD_DIR/tmp/
 make
+make install
+
 
 
 
